0ccc788215d576c3533a78961849eec68d412222
fozery <fozery@yandex.ru>
2024-05-18 11:43:49 +0300

    feat: change db to postgres

--- config/Database.php
+++ config/Database.php
@@ -3,9 +3,9 @@
 class Database
 {
     private $host = "db";
-    private $port = "3306";
-    private $db_name = "tagiev1_d";
-    private $username = "root";
+    private $port = "5432";
+    private $db_name = "pizza_delivery";
+    private $username = "postgres";
     private $password = "password";
     public $conn;
 
@@ -14,7 +14,7 @@ class Database
         $this->conn = null;
 
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("pgsql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name, $this->username, $this->password);
         } catch (PDOException $exception) {
             echo "Ошибка подключения: " . $exception->getMessage();
         }
