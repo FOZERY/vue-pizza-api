8c6f993a11edee390ee97ee93687db706f8291a3
fozery <fozery@yandex.ru>
2024-05-20 22:08:02 +0300

    fix: fix CORS

--- endpoints/product/create.php
+++ endpoints/product/create.php
@@ -1,13 +1,20 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: *");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Expose-Headers: *");
 
 include_once "../../config/Database.php";
 include_once "../../objects/Product.php";
 include_once "../../config/config.php";
+
+ if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+     http_response_code(200);
+     die();
+ }
 
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     http_response_code(405);
